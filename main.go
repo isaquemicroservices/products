a2572b65b36fe5290ac1827d935812d5d59c6f6b
isaqueveras <isaque.verasccb@gmail.com>
2021-12-01 11:52:15 -0300

    Init server gRPC

--- main.go
+++ main.go
@@ -1,3 +1,36 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net"
+
+	pb "github.com/isaqueveras/products-microservice/proto"
+	"google.golang.org/grpc"
+)
+
+// Server init server with gRPC
+type Server struct {
+	pb.UnimplementedProductsServer
+}
+
+func main() {
+	var (
+		listen net.Listener
+		err    error
+	)
+
+	// listen on port
+	if listen, err = net.Listen("tcp", ":9090"); err != nil {
+		log.Fatalf("Failed to listen: %v", err.Error())
+	}
+
+	// Creating new server
+	server := grpc.NewServer()
+
+	// Registering the server on grpc
+	pb.RegisterProductsServer(server, Server{})
+
+	if err = server.Serve(listen); err != nil {
+		log.Fatalf("Failed to server: %v", err.Error())
+	}
+}
